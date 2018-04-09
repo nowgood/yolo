ca59017fa3d182fe156d2bf966c5c520a66ef8d3
nowgood <851536944wb@gmail.com>
2018-04-09 15:01:43 +0800

    modify eval process

--- flower_net_eval.py
+++ flower_net_eval.py
@@ -12,15 +12,13 @@ import time
 cwd = os.path.dirname(os.path.abspath(__file__))
 print("文件目录: ", cwd)
 
-IMAGE_SIZE = 224
-NUMBER_OF_STEPS = 10000
-BATCH_SIZE = 128
 
-_DATA_URL = 'http://download.tensorflow.org/example_images/flower_photos.tgz'
 FLOWERS_DATA_DIR = os.path.join(cwd, 'datasets/flower_photos')
 TRAIN_DIR = os.path.join(cwd, 'model/train/flower_photos/')
 TRAIN_OR_VAL = 'validation'
 EVAL_DIR = os.path.join(cwd, 'model/eval/flower_photos/')
+
+BATCH_SIZE = 128
 
 
 def main(_):
